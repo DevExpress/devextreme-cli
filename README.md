41973950a42d81621a3fccdd81e6402b2458e385
Eugen Zha <nightskylark@users.noreply.github.com>
2018-08-16 16:27:26 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# DevExtreme CLI
+
+## Create new DevExtreme application
+
+### Angular
+```bash
+npx devextreme-cli new angular-app my-app
+```
+
+## Theme builder
+```bash
+npx build-theme --theme-name="generic.light" --metadata-file-path="./bootstrap.scss"
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').exec;
+const args = process.argv.slice(2);
+
+if(!args.length) {
+    console.log('No command found.');
+} else {
+    if(args[0] === 'new') {
+        if(!args[1]) {
+            console.log('No parameters found.');
+        } else {
+            if(args[1] === 'angular-app') {
+                const collectionIndex = args.indexOf('-c') + 1;
+                const collectionPath = collectionIndex ? args[collectionIndex] : 'devextreme-schematics';
+                const collectionName = collectionPath.replace(/^.*(\\|\/)/, '');
+                const appName = args[2] || 'my-app';
+                const cmd = `npx -p @angular-devkit/schematics-cli -p ${collectionPath} -c "schematics ${collectionName}:new-angular-app --name ${appName} --dry-run false"`;
+
+                exec(cmd, (error, stdout, stderr) => {
+                    stdout && console.log(stdout);
+                    stderr && console.log(stderr);
+                });
+            } else {
+                console.log(`Application type '${args[1]}' does not exist.` );
+            }
+        }
+    } else {
+        console.log(`Command '${args[0]}' does not exist.` );
+    }
+}
