79931f5db2b9c8d605d3d926c720c07f836319b5
DX Robot <DXGitHubRobot@devexpress.com>
2018-09-04 09:11:04 +0300

    Add a possibility to run a themebuilder

--- index.js
+++ index.js
@@ -5,33 +5,45 @@ const args = process.argv.slice(2);
 
 if(!args.length) {
     console.log('No command found.');
-} else {
-    if(args[0] === 'new') {
-        if(!args[1]) {
-            console.log('No parameters found.');
-        } else {
-            if(args[1] === 'angular-app') {
-                const collectionIndex = args.indexOf('-c') + 1;
-                const collectionPath = collectionIndex ? args[collectionIndex] : 'devextreme-schematics';
-                const collectionName = collectionPath.replace(/^.*(\\|\/)/, '');
-                const appName = args[2] || 'my-app';
-                const command = /^win/.test(process.platform) ? 'npx.cmd' : 'npx';
-                const commandArguments = [
-                    '-p', '@angular-devkit/schematics-cli',
-                    '-p', '@schematics/angular',
-                    '-p', collectionPath,
-                    '-c', `"schematics ${collectionName}:new-angular-app --name=${appName} --dry-run=false"`
-                ];
-
-                spawn(command, commandArguments, {
-                    stdio: 'inherit',
-                    windowsVerbatimArguments: true
-                });
-            } else {
-                console.log(`Application type '${args[1]}' does not exist.` );
-            }
-        }
-    } else {
-        console.log(`Command '${args[0]}' does not exist.` );
-    }
+    process.exit();
 }
+
+const run = arguments => {
+    const executable = /^win/.test(process.platform) ? 'npx.cmd' : 'npx';
+    spawn(executable, arguments, {
+        stdio: 'inherit',
+        windowsVerbatimArguments: true
+    });
+};
+
+const command = args[0];
+
+if(command === 'new') {
+    if(!args[1]) {
+        console.log('No parameters found.');
+    } else {
+        if(args[1] === 'angular-app') {
+            const collectionIndex = args.indexOf('-c') + 1;
+            const collectionPath = collectionIndex ? args[collectionIndex] : 'devextreme-schematics';
+            const collectionName = collectionPath.replace(/^.*(\\|\/)/, '');
+            const appName = args[2] || 'my-app';
+            
+            const commandArguments = [
+                '-p', '@angular-devkit/schematics-cli',
+                '-p', '@schematics/angular',
+                '-p', collectionPath,
+                '-c', `"schematics ${collectionName}:new-angular-app --name=${appName} --dry-run=false"`
+            ];
+
+            run(commandArguments);
+        } else {
+            console.log(`Application type '${args[1]}' does not exist.` );
+        }
+    }
+} else if(command === 'build-theme' || command === 'build-theme-vars') {
+    args.unshift('devextreme-themebuilder');
+    run(args);
+} else {
+    console.log(`Command '${command}' does not exist.` );
+}
+
