9a9ffe7951e9e84177555f7eb297c013084ec7d1
Eugen Zha <nightskylark@users.noreply.github.com>
2018-09-18 12:09:42 +0300

    Add commands considering schematics improvements (#5)

---
+++ commands/application.angular.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const runNpxCommand = require('../utility/run-npx-command');
+
+function runSchematicCommand(schematicCommand, options, evaluatingOptions) {
+    const collectionName = 'devextreme-schematics';
+    let collectionPath = collectionName;
+
+    if(options["c"]) {
+        collectionPath = path.join(process.cwd(), options["c"]);
+        delete options["c"];
+    }
+
+    const additionalOptions = [];
+    for(let option in options) {
+        additionalOptions.push(` --${option}="${options[option]}"`);
+    };
+
+    const commandArguments = [
+        '-p', '@angular-devkit/schematics-cli@latest',
+        '-p', '@schematics/angular@latest',
+        '-p', collectionPath,
+        '-c', `"schematics ${collectionName}:${schematicCommand}${additionalOptions.join('')} --dry-run=false"`
+    ];
+
+    runNpxCommand(commandArguments, evaluatingOptions);
+}
+
+const create = (appName, options) => {
+    runNpxCommand([
+        '-p', '@angular/cli@latest', 'ng', 'new', appName
+    ]).then(() => {
+        addTemplate(appName, options, {
+            cwd: path.join(process.cwd(), appName)
+        });
+    });
+};
+
+const addTemplate = (appName, options, evaluatingOptions) => {
+    runSchematicCommand(`add-app-template --project=${appName}`, options, evaluatingOptions);
+};
+
+const addView = (viewName, options) => {
+    runSchematicCommand(`add-view --name=${viewName}`, options);
+};
+
+module.exports = {
+    create,
+    addTemplate,
+    addView
+};
