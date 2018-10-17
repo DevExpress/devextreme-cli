648562860ded82cffad5adb5b9af432f22f9f01c
dxvladislavvolkov <dxvladislavvolkov@users.noreply.github.com>
2018-10-17 05:17:13 -0700

    Fix options with dash (#37)

--- commands/application.angular.js
+++ commands/application.angular.js
@@ -12,8 +12,11 @@ function runSchematicCommand(schematicCommand, options, evaluatingOptions) {
 
     const additionalOptions = [];
     for(let option in options) {
+        const optionName = option.replace(/(-)+(.)?/g, (match, separator, chr) => {
+            return chr ? chr.toUpperCase() : '';
+        });
         // NOTE: Removing boolean values is a workaround for the @angular/cli issue [angular/angular-cli#12150](https://github.com/angular/angular-cli/issues/12150)
-        const schematicOption = `--${option}${options[option] === true ? '' : `=${options[option]}`}`;
+        const schematicOption = `--${optionName}${options[option] === true ? '' : `=${options[option]}`}`;
         additionalOptions.push(schematicOption);
     };
 
