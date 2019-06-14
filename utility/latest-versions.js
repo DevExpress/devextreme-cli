15e0c865444de74f4c8f1416256bfd53f057b3fd
volkov.vladislav <dxvladislavvolkov@users.noreply.github.com>
2019-06-14 15:21:58 +0300

    Change versions to 19.1-stable (#98)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -1,7 +1,7 @@
 const packageJson = require('../package.json');
 module.exports = {
-    'devextreme': '~19.1.3',
-    'devextreme-react': '~19.1.3',
-    'devextreme-vue': '~19.1.3',
+    'devextreme': '19.1-stable',
+    'devextreme-react': '19.1-stable',
+    'devextreme-vue': '19.1-stable',
     'devextreme-cli': packageJson.version
 };
