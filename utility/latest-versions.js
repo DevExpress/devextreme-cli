6fbb36dc9d51cbe9d73bcf216ecb17ec5804be3d
Alexander Tkachenko <24324790+tar-z@users.noreply.github.com>
2019-10-29 13:28:33 +0300

    Update to devextreme 19.2 (#112)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -1,7 +1,7 @@
 const packageJson = require('../package.json');
 module.exports = {
-    'devextreme': '19.1-stable',
-    'devextreme-react': '19.1-stable',
-    'devextreme-vue': '19.1-stable',
+    'devextreme': '19.2-stable',
+    'devextreme-react': '19.2-stable',
+    'devextreme-vue': '19.2-stable',
     'devextreme-cli': packageJson.version
 };
