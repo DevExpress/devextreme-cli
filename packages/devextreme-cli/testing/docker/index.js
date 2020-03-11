a8049432b789ba8bb5567a466c03d13a3d4a8b02
ovchinnikov.andrey <ovchinnikov@users.noreply.github.com>
2020-03-11 10:53:16 +0300

    Docker config for local testing

---
+++ packages/devextreme-cli/testing/docker/index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+
+puppeteer.launch({
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--remote-debugging-port=9222',
+        '--remote-debugging-address=0.0.0.0',
+    ]
+});
