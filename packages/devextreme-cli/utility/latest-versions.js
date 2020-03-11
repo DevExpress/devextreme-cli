67cfa0f95a4caf6e7a54e0f9ce7336a6768c5c7d
ovchinnikov.andrey <ovchinnikov@users.noreply.github.com>
2020-03-11 18:00:13 +0300

    Downgrade dx-vue

--- packages/devextreme-cli/utility/latest-versions.js
+++ packages/devextreme-cli/utility/latest-versions.js
@@ -2,6 +2,6 @@ const packageJson = require('../package.json');
 module.exports = {
     'devextreme': '19.2-stable',
     'devextreme-react': '19.2-stable',
-    'devextreme-vue': '19.2-stable',
+    'devextreme-vue': '19.2.5',
     'devextreme-cli': packageJson.version
 };
