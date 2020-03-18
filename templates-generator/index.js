2a8e156104a710760f74f88c200a5f845d851578
egaluza <lizagaluza@mail.ru>
2020-03-18 11:47:56 +0300

    Refactor with linter

--- templates-generator/index.js
+++ templates-generator/index.js
@@ -2,78 +2,75 @@ const fs = require('fs');
 const buildOptions = require('minimist-options');
 const path = require('path');
 const glob = require('glob');
-const micromatch = require("micromatch");
+const micromatch = require('micromatch');
 const args = require('minimist')(process.argv.slice(2), buildOptions({
-  platform: {
-    type: 'string',
-    alias: 'p'
-  }
+    platform: {
+        type: 'string',
+        alias: 'p'
+    }
 }));
 
 const commands = args['_'];
 (() => {
-  if (commands.length) {
-    console.error(`Command is redundant`);
-    return;
-  }
-
-  const platformsConfigs = {
-    react: './react-config.js'
-  };
-
-  if (Object.keys(platformsConfigs).some(item => item === args.platform)) {
-    const config = require(platformsConfigs[args.platform], 'utf8');
-    generateTemplate(config);
-  }
-  else if (!args.platform) {
-    for (platform in platformsConfigs) {
-      const config = require(platformsConfigs[platform], 'utf8');
-      generateTemplate(config);
+    if(commands.length) {
+        console.error('Command is redundant');
+        return;
     }
-  }
-  else {
-    console.error(`Platform doesn't exist`);
-  }
 
-    function generateTemplate(config) {
-      const relativePaths = glob.sync(config.sourceGlob, {
-        cwd: config.sourcePath,
-        ignore: config.ignoreList
-      });
-      relativePaths.forEach(relativePath => {
-        let content = fs.readFileSync(`${config.sourcePath}${relativePath}`, 'utf8');
-        content = updateContent(relativePath, content, config.updateInfo);
+    const platformsConfigs = {
+        react: './react-config.js'
+    };
 
-        writeFile(relativePath, content, config);
-      });
+    if(Object.keys(platformsConfigs).some(item => item === args.platform)) {
+        generateTemplate(args.platform);
+    } else if(!args.platform) {
+        for(let platform in platformsConfigs) {
+            generateTemplate(platform);
+        }
+    } else {
+        console.error('Platform doesn\'t exist');
+    }
+
+    function generateTemplate(platform) {
+        const config = require(platformsConfigs[platform], 'utf8');
+        const relativePaths = glob.sync(config.sourceGlob, {
+            cwd: config.sourcePath,
+            ignore: config.ignoreList
+        });
+        relativePaths.forEach(relativePath => {
+            let content = fs.readFileSync(`${config.sourcePath}${relativePath}`, 'utf8');
+            content = updateContent(relativePath, content, config.updateInfo);
+
+            writeFile(relativePath, content, config);
+        });
     }
 
     function updateContent(relativePath, content, updateInfo) {
-      const rules = updateInfo.filter(info => micromatch.isMatch(relativePath, info.pattern));
-      rules.forEach(rule => {
-        rule.definitions.forEach(definition => {
-          content = content.replace(definition.before, definition.after);
-        })
-      })
+        const rules = updateInfo.filter(info => micromatch.isMatch(relativePath, info.pattern));
+        rules.forEach(rule => {
+            rule.definitions.forEach(definition => {
+                content = content.replace(definition.before, definition.after);
+            });
+        });
 
-      return content;
+        return content;
     }
 
     function writeFile(relativePath, content, { moveRules, targetPath }) {
-      const rule = moveRules.find(rule => micromatch.isMatch(relativePath, rule.pattern));
-      const fullPath = rule
-        ? `${rule.definition.targetPath}${relativePath.replace(rule.definition.sourcePath, '')}`
-        : `${targetPath}${relativePath}`;
+        const rule = moveRules.find(rule => micromatch.isMatch(relativePath, rule.pattern));
+        const fullPath = rule
+            ? `${rule.definition.targetPath}${relativePath.replace(rule.definition.sourcePath, '')}`
+            : `${targetPath}${relativePath}`;
 
-      createNestedFolder(fullPath, content);
-      fs.writeFileSync(fullPath, content);
+        createNestedFolder(fullPath, content);
+        fs.writeFileSync(fullPath, content);
     }
 
     function createNestedFolder(fullPath) {
-      const dirName = path.dirname(fullPath);
-      if (!fs.existsSync(dirName)) {
-        fs.mkdirSync(dirName, { recursive: true });
-      }
+        const dirName = path.dirname(fullPath);
+        if(!fs.existsSync(dirName)) {
+            fs.mkdirSync(dirName, { recursive: true });
+        }
     }
 
     process.exit();
--- templates-generator/react-config.js
+++ templates-generator/react-config.js
@@ -1,132 +1,132 @@
 module.exports = {
-  sourcePath: 'packages/devextreme-cli/testing/sandbox/react/my-app/',
-  targetPath: 'packages/devextreme-cli/templates/react/application/',
-  sourceGlob: '**/*.{js,scss,json}',
-  updateInfo: [
-    {
-      pattern: '**/src/app-routes.js',
-      definitions: [
+    sourcePath: 'packages/devextreme-cli/testing/sandbox/react/my-app/',
+    targetPath: 'packages/devextreme-cli/templates/react/application/',
+    sourceGlob: '**/*.{js,scss,json}',
+    updateInfo: [
         {
-          before: 'import',
-          after: '<%=^empty%>import'
+            pattern: '**/src/app-routes.js',
+            definitions: [
+                {
+                    before: 'import',
+                    after: '<%=^empty%>import'
+                },
+                {
+                    before: '];',
+                    after: '<%=/empty%>];'
+                },
+                {
+                    before: 'export default [',
+                    after: '<%=/empty%>export default [<%=^empty%>'
+                }
+            ],
         },
         {
-          before: '];',
-          after: '<%=/empty%>];'
+            pattern: '**/src/app-info.js',
+            definitions: [{
+                before: 'My App',
+                after: '<%=project%>'
+            }
+            ],
         },
         {
-          before: 'export default [',
-          after: '<%=/empty%>export default [<%=^empty%>'
+            pattern: '**/src/app-navigation.js',
+            definitions: [
+                {
+                    before: '= [',
+                    after: '= [<%=^empty%>'
+                },
+                {
+                    before: '];',
+                    after: '<%=/empty%>];'
+                }
+            ]
+        },
+        {
+            pattern: '**/src/App.js',
+            definitions: [
+                {
+                    before: 'SideNavOuterToolbar',
+                    after: '<%=layout%>'
+                },
+                {
+                    before: 'SideNavInnerToolbar',
+                    after: '<%=layout%>'
+                },
+                {
+                    before: 'import \'devextreme/dist/css/dx.common.css\';\n',
+                    after: ''
+                },
+                {
+                    before: 'import \'./themes/generated/theme.base.css\';\n',
+                    after: ''
+                },
+                {
+                    before: 'import \'./themes/generated/theme.additional.css\';\n',
+                    after: ''
+                },
+                {
+                    before: '))}',
+                    after: '))}<%=^empty%>'
+                },
+                {
+                    before: '<Redirect to={\'/home\'} />',
+                    after: '<Redirect to={\'/home\'} /><%=/empty%>'
+                }
+            ]
+        },
+        {
+            pattern: '**/themes/metadata.additional.json',
+            definitions: [
+                {
+                    before: /"baseTheme": ".*?"/,
+                    after: '"baseTheme": "material.orange.dark"'
+                }
+            ]
+        },
+        {
+            pattern: '**/themes/metadata.base.json',
+            definitions: [
+                {
+                    before: /"baseTheme": ".*?"/,
+                    after: '"baseTheme": "material.orange.light"'
+                }
+            ]
+        },
+        {
+            pattern: '**/themes/metadata.**.json',
+            definitions: [
+                {
+                    before: /"items":\s+\[[^]*?\]/,
+                    after: '"items": []'
+                }
+            ]
         }
-      ],
-    },
-    {
-      pattern: '**/src/app-info.js',
-      definitions: [{
-        before: 'My App',
-        after: '<%=project%>'
-      }
-      ],
-    },
-    {
-      pattern: '**/src/app-navigation.js',
-      definitions: [
+    ],
+    moveRules: [
         {
-          before: '= [',
-          after: '= [<%=^empty%>'
+            pattern: '**/src/pages/**/*.*',
+            definition: {
+                sourcePath: 'src/pages',
+                targetPath: 'packages/devextreme-cli/templates/react/sample-pages'
+            }
         },
         {
-          before: '];',
-          after: '<%=/empty%>];'
+            pattern: '**/devextreme.json',
+            definition: {
+                sourcePath: '',
+                targetPath: 'packages/devextreme-cli/templates/react/application/'
+            }
         }
-      ]
-    },
-    {
-      pattern: '**/src/App.js',
-      definitions: [
-        {
-          before: 'SideNavOuterToolbar',
-          after: '<%=layout%>'
-        },
-        {
-          before: 'SideNavInnerToolbar',
-          after: '<%=layout%>'
-        },
-        {
-          before: 'import \'devextreme/dist/css/dx.common.css\';\n',
-          after: ''
-        },
-        {
-          before: 'import \'./themes/generated/theme.base.css\';\n',
-          after: ''
-        },
-        {
-          before: 'import \'./themes/generated/theme.additional.css\';\n',
-          after: ''
-        },
-        {
-          before: '))}',
-          after: '))}<%=^empty%>'
-        },
-        {
-          before: '<Redirect to={\'/home\'} />',
-          after: '<Redirect to={\'/home\'} /><%=/empty%>'
-        }
-      ]
-    },
-    {
-      pattern: '**/themes/metadata.additional.json',
-      definitions: [
-        {
-          before: /"baseTheme": ".*?"/,
-          after: '"baseTheme": "material.orange.dark"'
-        }
-      ]
-    },
-    {
-      pattern: '**/themes/metadata.base.json',
-      definitions: [
-        {
-          before: /"baseTheme": ".*?"/,
-          after: '"baseTheme": "material.orange.light"'
-        }
-      ]
-    },
-    {
-      pattern: '**/themes/metadata.**.json',
-      definitions: [
-        {
-          before: /"items":\s+\[[^]*?\]/,
-          after: '"items": []'
-        }
-      ]
-    }
-  ],
-  moveRules: [
-    {
-      pattern: '**/src/pages/**/*.*',
-      definition: {
-        sourcePath: 'src/pages',
-        targetPath: 'packages/devextreme-cli/templates/react/sample-pages'
-      }
-    },
-    {
-      pattern: '**/devextreme.json',
-      definition: {
-        sourcePath: '',
-        targetPath: 'packages/devextreme-cli/templates/react/application/'
-      }
-    }
-  ],
-  ignoreList: [
-    '**/themes/generated/**/*.*',
-    '**/src/App.test.js',
-    '**/src/setupTests.js',
-    '**/src/serviceWorker.js',
-    '**/src/index.js',
-    '**/node_modules/**/*.*',
-    '**/public/**/*.*',
-    'package.json',
-    'package-lock.json'
-  ]
-}
+    ],
+    ignoreList: [
+        '**/themes/generated/**/*.*',
+        '**/src/App.test.js',
+        '**/src/setupTests.js',
+        '**/src/serviceWorker.js',
+        '**/src/index.js',
+        '**/node_modules/**/*.*',
+        '**/public/**/*.*',
+        'package.json',
+        'package-lock.json'
+    ]
+};
