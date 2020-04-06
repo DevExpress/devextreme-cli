bc2d96856322b9faae727c78eda30bb7674fa024
tar-z <24324790+tar-z@users.noreply.github.com>
2020-04-06 12:32:22 +0300

    Add change password form

--- packages/devextreme-cli/templates/react/application/src/NotAuthenticatedContent.js
+++ packages/devextreme-cli/templates/react/application/src/NotAuthenticatedContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { SingleCard } from './layouts';
-import { LoginForm, ResetPasswordForm, CreateAccountForm } from './components';
+import { LoginForm, ResetPasswordForm, ChangePasswordForm, CreateAccountForm } from './components';
 
 export default function () {
   return (
@@ -22,6 +22,11 @@ export default function () {
           description="Please enter the email address that you used to register, and we will send you an email with a link to reset your password."
         >
           <ResetPasswordForm />
+        </SingleCard>
+      </Route>
+      <Route exact path='/change-password/:recoveryCode' >
+        <SingleCard title="Change Password">
+          <ChangePasswordForm />
         </SingleCard>
       </Route>
       <Redirect to={'/login'} />
