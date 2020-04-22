894ba5bc8e108dbd5e1ac64878380692847672c8
egaluza <lizagaluza@mail.ru>
2020-04-22 23:39:17 +0300

    Fix routing after login

--- packages/devextreme-schematics/src/add-layout/files/src/app/shared/services/auth.service.ts
+++ packages/devextreme-schematics/src/add-layout/files/src/app/shared/services/auth.service.ts
@@ -3,40 +3,45 @@ import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
 
 @Injectable()
 export class AuthService {
-  loggedIn = true;
+    loggedIn = true;
+    prevPath: string;
+    constructor(private router: Router) { }
 
-  constructor(private router: Router) {}
+    logIn(login: string, password: string) {
+        this.loggedIn = true;
+        this.router.navigate([this.prevPath]);
+    }
 
-  logIn(login: string, passord: string) {
-    this.loggedIn = true;
-    this.router.navigate(['/']);
-  }
+    logOut() {
+        this.loggedIn = false;
+        this.router.navigate(['/login-form']);
+    }
 
-  logOut() {
-    this.loggedIn = false;
-    this.router.navigate(['/login-form']);
-  }
-
-  get isLoggedIn() {
-    return this.loggedIn;
-  }
+    get isLoggedIn() {
+        return this.loggedIn;
+    }
 }
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-    constructor(private router: Router, private authService: AuthService) {}
+    constructor(private router: Router, private authService: AuthService) { }
 
     canActivate(route: ActivatedRouteSnapshot): boolean {
         const isLoggedIn = this.authService.isLoggedIn;
         const isLoginForm = route.routeConfig.path === 'login-form';
 
         if (isLoggedIn && isLoginForm) {
+            this.authService.prevPath = '/'
             this.router.navigate(['/']);
             return false;
         }
 
         if (!isLoggedIn && !isLoginForm) {
             this.router.navigate(['/login-form']);
+        }
+
+        if (isLoggedIn) {
+            this.authService.prevPath = route.routeConfig.path;
         }
 
         return isLoggedIn || isLoginForm;
