6472c9e4af36117de605c09b2d575fa94b5ef348
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2020-04-27 12:04:18 +0300

    Fix tests

--- packages/devextreme-cli/src/templates/react/sample-pages/home/home.js
+++ packages/devextreme-cli/src/templates/react/sample-pages/home/home.js
@@ -21,12 +21,12 @@ export default () => (
 
         <p>Welcome to <b>{appInfo.title}</b>!</p><p>The application is built with:</p>
         <ul>
-          <li><a href={'https://create-react-app.dev/'} target={'_blank'}>Create React App</a></li>
-          <li><a href={'https://js.devexpress.com/Documentation/Guide/Getting_Started/DevExtreme_CLI/'} target={'_blank'}>DevExtreme CLI</a></li>
-          <li><a href={'https://js.devexpress.com/Documentation/Guide/React_Components/DevExtreme_React_Components/'} target={'_blank'}>DevExtreme-React</a> components</li>
+          <li><a href={'https://create-react-app.dev/'} target='_blank'>Create React App</a></li>
+          <li><a href={'https://js.devexpress.com/Documentation/Guide/Getting_Started/DevExtreme_CLI/'} target='_blank'>DevExtreme CLI</a></li>
+          <li><a href={'https://js.devexpress.com/Documentation/Guide/React_Components/DevExtreme_React_Components/'} target='_blank'>DevExtreme-React</a> components</li>
         </ul>
 
-        <p>The application layouts are implemented using the <a href={'https://js.devexpress.com/Documentation/ApiReference/UI_Widgets/dxDrawer/'} target={'_blank'}>Drawer</a> component. Several component examples are also available in the <b>Examples</b> section. For complete information on DevExtreme, check our <a href={'https://js.devexpress.com/Documentation/'} target={'_blank'}>documentation</a> and <a href={'https://js.devexpress.com/Demos/WidgetsGallery/'} target={'_blank'}>demos</a>.</p>
+        <p>The application layouts are implemented using the <a href={'https://js.devexpress.com/Documentation/ApiReference/UI_Widgets/dxDrawer/'} target='_blank'>Drawer</a> component. Several component examples are also available in the <b>Examples</b> section. For complete information on DevExtreme, check our <a href={'https://js.devexpress.com/Documentation/'} target='_blank'>documentation</a> and <a href={'https://js.devexpress.com/Demos/WidgetsGallery/'} target='_blank'>demos</a>.</p>
         <p>You can do the following to create a custom application using this template as a base point:</p>
 
         <ul>
@@ -38,7 +38,7 @@ export default () => (
           <li><b>Switch the layout</b></li>
         </ul>
 
-        <p>Please, read README in <a href={'https://github.com/DevExpress/devextreme-react-template/blob/master/README.md'} target={'_blank'}>devextreme-react-template</a> GitHub repository for more information.</p>
+        <p>Please, read README in <a href={'https://github.com/DevExpress/devextreme-react-template/blob/master/README.md'} target='_blank'>devextreme-react-template</a> GitHub repository for more information.</p>
       </div>
     </div>
   </React.Fragment>
