564561ce7bb599a4464acead5517dd1611f63841
egaluza <lizagaluza@mail.ru>
2020-04-28 13:52:09 +0300

    Formatting

--- packages/devextreme-schematics/src/add-layout/files/src/app/shared/services/auth.service.ts
+++ packages/devextreme-schematics/src/add-layout/files/src/app/shared/services/auth.service.ts
@@ -5,51 +5,51 @@ const defaultPath = '/';
 
 @Injectable()
 export class AuthService {
-    private _loggedIn: boolean = true;
-    get loggedIn(): boolean {
-        return this._loggedIn;
-    }
+  private _loggedIn: boolean = true;
+  get loggedIn(): boolean {
+    return this._loggedIn;
+  }
 
-    private _lastAuthenticatedPath: string = defaultPath;
-    set lastAuthenticatedPath(value: string) {
-        this._lastAuthenticatedPath = value;
-    }
+  private _lastAuthenticatedPath: string = defaultPath;
+  set lastAuthenticatedPath(value: string) {
+    this._lastAuthenticatedPath = value;
+  }
 
-    constructor(private router: Router) { }
+  constructor(private router: Router) { }
 
-    logIn(login: string, password: string) {
-        this._loggedIn = true;
-        this.router.navigate([this._lastAuthenticatedPath]);
-    }
+  logIn(login: string, password: string) {
+    this._loggedIn = true;
+    this.router.navigate([this._lastAuthenticatedPath]);
+  }
 
-    logOut() {
-        this._loggedIn = false;
-        this.router.navigate(['/login-form']);
-    }
+  logOut() {
+    this._loggedIn = false;
+    this.router.navigate(['/login-form']);
+  }
 }
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-    constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
-    canActivate(route: ActivatedRouteSnapshot): boolean {
-        const isLoggedIn = this.authService.loggedIn;
-        const isLoginForm = route.routeConfig.path === 'login-form';
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const isLoggedIn = this.authService.loggedIn;
+    const isLoginForm = route.routeConfig.path === 'login-form';
 
-        if (isLoggedIn && isLoginForm) {
-            this.authService.lastAuthenticatedPath = defaultPath;
-            this.router.navigate([defaultPath]);
-            return false;
-        }
-
-        if (!isLoggedIn && !isLoginForm) {
-            this.router.navigate(['/login-form']);
-        }
-
-        if (isLoggedIn) {
-            this.authService.lastAuthenticatedPath = route.routeConfig.path;
-        }
-
-        return isLoggedIn || isLoginForm;
+    if(isLoggedIn && isLoginForm) {
+      this.authService.lastAuthenticatedPath = defaultPath;
+      this.router.navigate([defaultPath]);
+      return false;
     }
+
+    if(!isLoggedIn && !isLoginForm) {
+      this.router.navigate(['/login-form']);
+    }
+
+    if(isLoggedIn) {
+      this.authService.lastAuthenticatedPath = route.routeConfig.path;
+    }
+
+    return isLoggedIn || isLoginForm;
+  }
 }
