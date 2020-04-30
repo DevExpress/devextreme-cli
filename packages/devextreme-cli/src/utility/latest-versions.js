b9c146ab72fb90a96ff71691b909be1485979f51
volkov.vladislav <dxvladislavvolkov@users.noreply.github.com>
2020-04-30 15:06:48 +0300

    Return current version (#191)

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,7 +1,8 @@
+const packageJson = require('../../package.json');
 module.exports = {
     'devextreme': '20.1-stable',
     'devextreme-react': '20.1-stable',
     'devextreme-vue': '20.1-stable',
-    'devextreme-cli': 'latest',
+    'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
