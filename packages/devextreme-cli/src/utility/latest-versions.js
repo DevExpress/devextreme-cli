d23798b40c31d569608bdc43127d490ae28af2f5
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2020-04-30 13:07:16 +0300

    Use latest cli version

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -3,6 +3,6 @@ module.exports = {
     'devextreme': '20.1-stable',
     'devextreme-react': '20.1-stable',
     'devextreme-vue': '20.1-stable',
-    'devextreme-cli': packageJson.version,
+    'devextreme-cli': 'latest',
     'devextreme-schematics': 'latest'
 };
