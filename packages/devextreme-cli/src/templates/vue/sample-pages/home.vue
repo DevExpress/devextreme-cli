13a554d9f3c800742de640ace14946397bfd1b60
egaluza <51698893+egaluza@users.noreply.github.com>
2020-07-20 22:40:11 +0300

    Add missing spaces and periods in home page texts (#218)

--- packages/devextreme-cli/src/templates/vue/sample-pages/home.vue
+++ packages/devextreme-cli/src/templates/vue/sample-pages/home.vue
@@ -92,8 +92,8 @@
         <p>
           Thanks for using the DevExtreme Vue App Template.
         </p>
-        <p>This application was built using <a href="https://cli.vuejs.org/" target="_blank" rel="noopener noreferrer"> Vue CLI </a> 
-          and <a href="https://js.devexpress.com/Documentation/Guide/Common/DevExtreme_CLI/" target="_blank" rel="noopener noreferrer"> DevExtreme CLI </a>
+        <p>This application was built using <a href="https://cli.vuejs.org/" target="_blank" rel="noopener noreferrer">Vue CLI</a> 
+          and <a href="https://js.devexpress.com/Documentation/Guide/Common/DevExtreme_CLI/" target="_blank" rel="noopener noreferrer">DevExtreme CLI</a>
           and includes the following DevExtreme components:</p>
         <ul>
           <li>
@@ -125,11 +125,10 @@
             <a href="https://js.devexpress.com/Documentation/Guide/Vue_Components/Create_a_DevExtreme_Application/#Configure_Themes" target="_blank" rel="noopener noreferrer">Configure Themes</a>
           </li>
         </ul>
-        <p>
-          For technical content related to DevExtreme Vue components, feel free to explore our 
-          <a href="https://js.devexpress.com/documentation/" target="_blank" rel="noopener noreferrer">online documentation </a>
-          and
-          <a href="https://js.devexpress.com/Demos/Widgetsgallery/" target="_blank" rel="noopener noreferrer">technical demos</a>
+         <p>
+          For technical content related to DevExtreme Vue components, feel free to explore 
+          our <a href="https://js.devexpress.com/documentation/" target="_blank" rel="noopener noreferrer">online documentation</a>
+          and <a href="https://js.devexpress.com/Demos/Widgetsgallery/" target="_blank" rel="noopener noreferrer">technical demos</a>.
         </p>
       </div>
     </div>
