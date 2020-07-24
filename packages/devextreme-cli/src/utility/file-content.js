d228d0a970a38d1a3de05bb29f8a1bf6f6cf4fb4
egaluza <lizagaluza@mail.ru>
2020-07-24 14:44:22 +0300

    Change regular expression

--- packages/devextreme-cli/src/utility/file-content.js
+++ packages/devextreme-cli/src/utility/file-content.js
@@ -5,7 +5,7 @@ const isEmptyRouting = (content) => {
 };
 
 const getPositionIndex = (content) => {
-    const edgeIndex = content.search(/\s+(\S+)?](;|(\s}\);))/);
+    const edgeIndex = content.search(/\n[^\[,^}]*](;|[^\]]*;)/);
 
     if(edgeIndex === -1) {
         console.error('No route found.');
