145527aa9fc6d3d0cbec75bbccc9cf5d04e94a0f
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2020-08-14 13:46:31 +0300

    Fix lint

--- packages/devextreme-cli/testing/jest-setup.js
+++ packages/devextreme-cli/testing/jest-setup.js
@@ -1,5 +1,4 @@
 // eslint runs from root and only looks at root package.json
-// eslint-disable-next-line import/no-unresolved
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
 expect.extend({ toMatchImageSnapshot });
