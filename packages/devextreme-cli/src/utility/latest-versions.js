7f1f021d6a8a8f590cdb604e03190c14bd8fb9a0
Alexey Babich <alexey.babych@gmail.com>
2020-10-19 11:04:39 +0300

    Fix variables export (#248)

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '20.1-stable',
-    'devextreme-react': '20.1-stable',
-    'devextreme-vue': '20.1-stable',
+    'devextreme': '20.1.7',
+    'devextreme-react': '20.1.7',
+    'devextreme-vue': '20.1.7',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
