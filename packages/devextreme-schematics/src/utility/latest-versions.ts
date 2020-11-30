534c3f311cc8d5f194277d50ba806cdb412311e4
Elizaveta Galuza (DevExpress) <galuza.elizaveta@corp.devexpress.com>
2020-11-30 19:00:52 +0300

    Change snapshots

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '^20.2.3',
+  'devextreme': '20.2.4-build-20330-1811',
   'devextreme-angular': '^20.2.3',
   'devextreme-cli': 'latest'
 };
