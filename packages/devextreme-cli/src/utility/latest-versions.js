a1213efb0564aa1b13bbde40362e445358698744
babich-a <alexey.babych@gmail.com>
2020-12-03 14:13:55 +0300

    Update package versions (20.2.4)

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '20.2.4-build-20330-1811',
-    'devextreme-react': '^20.2.3',
-    'devextreme-vue': '^20.2.3',
+    'devextreme': '^20.2.4',
+    'devextreme-react': '^20.2.4',
+    'devextreme-vue': '^20.2.4',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
