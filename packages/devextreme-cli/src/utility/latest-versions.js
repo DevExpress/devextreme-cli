e6972d4f24623786167c024c99cf33c5e626b184
babich-a <alexey.babych@gmail.com>
2020-12-03 16:02:26 +0300

    downgrade react

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,7 +1,7 @@
 const packageJson = require('../../package.json');
 module.exports = {
     'devextreme': '^20.2.4',
-    'devextreme-react': '^20.2.4',
+    'devextreme-react': '20.2.3',
     'devextreme-vue': '^20.2.4',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
