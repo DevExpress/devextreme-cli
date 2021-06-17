863ebe8089c27ffa713d569bb181408e7ebdf4af
babich-a <alexey.babych@gmail.com>
2021-06-17 13:10:14 +0300

    change log location

--- packages/devextreme-cli/testing/dev-server.js
+++ packages/devextreme-cli/testing/dev-server.js
@@ -5,6 +5,7 @@ const kill = require('tree-kill-promise');
 
 const runCommand = require('../src/utility/run-command');
 const { themes, swatchModes, baseFontFamily } = require('./constants');
+const logsDirPath = path.join(process.cwd(), 'testing', 'sandbox', 'logs');
 
 module.exports = class DevServer {
     constructor(env) {
@@ -15,7 +16,7 @@ module.exports = class DevServer {
         fs.mkdirSync(this.env.deployPath, { recursive: true });
 
         const command = /^win/.test(process.platform) ? 'npx.cmd' : 'npx';
-        this.devServerProcess = spawn(command, ['http-server', this.env.deployPath, '-c-1', '>>', 'http-server.log']);
+        this.devServerProcess = spawn(command, ['http-server', this.env.deployPath, '-c-1', '>>', path.join(logsDirPath, 'http-server.log')]);
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if(this.devServerProcess.exitCode === null) resolve();
@@ -38,7 +39,7 @@ module.exports = class DevServer {
                 // https://github.com/facebook/create-react-app/issues/3657
                 env: Object.assign(process.env, { CI: false })
             });
-            const logsDirPath = path.join(process.cwd(), './testing/sandbox/logs');
+
             fs.mkdirSync(logsDirPath, { recursive: true });
 
             const logFilePath = path.join(logsDirPath, `${this.env.engine}.log`);
