737ab579096f6f1b4360445d53029f96db216052
babich-a <alexey.babych@gmail.com>
2021-07-27 08:48:43 +0300

    fix regular expression, add node 16

--- packages/devextreme-cli/testing/themebuilder/install-local.test.js
+++ packages/devextreme-cli/testing/themebuilder/install-local.test.js
@@ -24,7 +24,7 @@ const parseDetectedVersion = (output) => {
     return output.replace(/.*Using version (\d\d\.\d.\d\d?|latest).*/s, '$1');
 };
 const parseInstalledVersion = (output) => {
-    const reg = /.*> npm.cmd install --no-save devextreme-themebuilder@(\d\d\.\d.\d\d?).*/s;
+    const reg = /.*> npm(\.cmd)? install --no-save devextreme-themebuilder@(\d\d\.\d.\d\d?).*/s;
     if(!reg.test(output)) return null;
     return output.replace(reg, '$1');
 };
