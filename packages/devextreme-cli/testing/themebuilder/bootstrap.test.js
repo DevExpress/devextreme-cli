7f3f68b729c6c32331a79ceb3c7b406c655ae6ea
babich-a <alexey.babych@gmail.com>
2021-07-30 13:27:59 +0300

    add bootstrap 4, 5 tests

--- packages/devextreme-cli/testing/themebuilder/bootstrap.test.js
+++ packages/devextreme-cli/testing/themebuilder/bootstrap.test.js
@@ -8,9 +8,9 @@ const {
     getVersionFromTheme
 } = require('./utils');
 
-const workDirectory = join(__dirname, 'bootstrap3');
+const workDirectory = join(__dirname, 'bootstrap');
 
-describe('ThemeBuilder bootstrap3 integration tests', () => {
+describe('ThemeBuilder bootstrap integration tests', () => {
     jest.setTimeout(1800000); // 30min
     beforeAll(async() => {
         await buildPackage();
@@ -31,7 +31,7 @@ describe('ThemeBuilder bootstrap3 integration tests', () => {
         await rm(join(workDirectory, 'node_modules'), { recursive: true, force: true });
     });
 
-    test('Empty less file', async() => {
+    test('Empty less file (bootstrap 3)', async() => {
         await writeFile(join(workDirectory, 'bootstrap3.less'), '');
 
         const runResult = spawnSync('node', [
@@ -48,13 +48,56 @@ describe('ThemeBuilder bootstrap3 integration tests', () => {
         expect(buildedVersion).toBe(realInstalledVersion);
     });
 
-    test('Change primary color', async() => {
+    test('Change primary color (bootstrap 3)', async() => {
         await writeFile(join(workDirectory, 'bootstrap3.less'), '@brand-primary: #123456;\n');
 
         const runResult = spawnSync('node', [
             join(workDirectory, 'node_modules', 'devextreme-cli', 'index.js'),
             'build-theme',
             '--input-file=bootstrap3.less'
+        ], { cwd: workDirectory }).stdout.toString();
+
+        console.log('Run result\n', runResult);
+
+        const buildedVersion = await getVersionFromTheme(workDirectory);
+        const realInstalledVersion = getLatestThemeBuilderVersion();
+
+        const themeFileName = join(workDirectory, 'dx.generic.custom-scheme.css');
+        const themeContent = await readFile(themeFileName, 'utf8');
+
+        expect(buildedVersion).toBe(realInstalledVersion);
+        expect(themeContent.indexOf('123456') !== -1).toBe(true);
+    });
+
+    test('Change primary color (bootstrap 4)', async() => {
+        await writeFile(join(workDirectory, 'bootstrap4.scss'), '$primary: #123456;\n');
+
+        const runResult = spawnSync('node', [
+            join(workDirectory, 'node_modules', 'devextreme-cli', 'index.js'),
+            'build-theme',
+            '--input-file=bootstrap4.scss'
+        ], { cwd: workDirectory }).stdout.toString();
+
+        console.log('Run result\n', runResult);
+
+        const buildedVersion = await getVersionFromTheme(workDirectory);
+        const realInstalledVersion = getLatestThemeBuilderVersion();
+
+        const themeFileName = join(workDirectory, 'dx.generic.custom-scheme.css');
+        const themeContent = await readFile(themeFileName, 'utf8');
+
+        expect(buildedVersion).toBe(realInstalledVersion);
+        expect(themeContent.indexOf('123456') !== -1).toBe(true);
+    });
+
+    test('Change primary color (bootstrap 5)', async() => {
+        await writeFile(join(workDirectory, 'bootstrap5.scss'), '$primary: #123456;\n');
+
+        const runResult = spawnSync('node', [
+            join(workDirectory, 'node_modules', 'devextreme-cli', 'index.js'),
+            'build-theme',
+            '--input-file=bootstrap5.scss',
+            '--bootstrap-version=5'
         ], { cwd: workDirectory }).stdout.toString();
 
         console.log('Run result\n', runResult);
