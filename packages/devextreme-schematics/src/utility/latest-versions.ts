d6c7bad7690c89c27198f4f8255b5e031b52cb59
babich-a <alexey.babych@gmail.com>
2021-11-12 07:03:58 +0300

    update versions

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '^21.1.5',
-  'devextreme-angular': '^21.1.5',
+  'devextreme': '^21.2.3',
+  'devextreme-angular': '^21.2.3',
   'devextreme-cli': 'latest'
 };
