14ed8a7cf316c9436fc92a8838dec92241376a08
egaluza <lizagaluza@mail.ru>
2021-12-09 14:17:26 +0300

    Update devextreme versions

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '^21.2.3',
-    'devextreme-react': '^21.2.3',
-    'devextreme-vue': '^21.2.3',
+    'devextreme': '^21.2.4',
+    'devextreme-react': '^21.2.4',
+    'devextreme-vue': '^21.2.4',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
