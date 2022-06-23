671821786da2042163228afa7eace295f26c98e2
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2022-06-23 12:46:16 +0400

    Bump devextreme version

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '^21.2.4',
-  'devextreme-angular': '^21.2.4',
+  'devextreme': '^22.1.3',
+  'devextreme-angular': '^22.1.3',
   'devextreme-cli': 'latest'
 };
