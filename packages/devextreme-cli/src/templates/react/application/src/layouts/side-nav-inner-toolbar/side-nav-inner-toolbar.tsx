f09dc9386ee62aae290c0e22e9e677309b080fb6
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-09-02 10:33:04 +0400

    apply for inner

--- packages/devextreme-cli/src/templates/react/application/src/layouts/side-nav-inner-toolbar/side-nav-inner-toolbar.tsx
+++ packages/devextreme-cli/src/templates/react/application/src/layouts/side-nav-inner-toolbar/side-nav-inner-toolbar.tsx
@@ -45,7 +45,7 @@ export default function SideNavInnerToolbar({ title, children }<%=#isTypeScript%
         ? MenuStatus.Closed
         : prevMenuStatus
     );
-    return true;
+    return menuStatus === MenuStatus.Closed ? true : false;
   }, [isLarge]);
 
   const onNavigationChanged = useCallback(({ itemData, event, node }<%=#isTypeScript%>: ItemClickEvent<%=/isTypeScript%>) => {
