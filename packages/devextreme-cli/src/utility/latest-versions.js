d369568cb3732fc5bc1188b45c49fcd999aded13
timbset <timbset@gmail.com>
2023-06-08 17:56:34 +0400

    Try to downgrade devextreme version

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '23.1.2-beta.2',
-    'devextreme-react': '23.1.2-beta.2',
-    'devextreme-vue': '23.1.2-beta.2',
+    'devextreme': '23.1.2-alpha-23114-0308',
+    'devextreme-react': '23.1.2-alpha-23114-0308',
+    'devextreme-vue': '23.1.2-alpha-23114-0308',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '23.1.2-beta.2',
-  'devextreme-angular': '23.1.2-beta.2',
+  'devextreme': '23.1.2-alpha-23114-0308',
+  'devextreme-angular': '23.1.2-alpha-23114-0308',
   'devextreme-cli': 'latest'
 };
