2f0b1719c3091898c12a7efc03c4a584d9c352a3
Alexander Bulychev <git@bulychev.pro>
2023-06-15 16:31:11 +0400

    research puppeteer

--- packages/devextreme-cli/testing/docker/index.js
+++ packages/devextreme-cli/testing/docker/index.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 
 puppeteer.launch({
     headless: 'new',
+    executablePath: process.env.CHROME_PATH,
     args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
