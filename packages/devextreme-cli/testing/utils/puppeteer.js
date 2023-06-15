149fcda911fae7915db280a2eae3f024f4352848
Alexander Bulychev <git@bulychev.pro>
2023-06-15 16:51:29 +0400

    research puppeteer

--- packages/devextreme-cli/testing/utils/puppeteer.js
+++ packages/devextreme-cli/testing/utils/puppeteer.js
@@ -3,7 +3,9 @@ const puppeteer = require('puppeteer');
 module.exports.getBrowser = () => {
     if(Boolean(process.env.LAUNCH_BROWSER)) {
         return puppeteer.launch({
-            args: ['--no-sandbox']
+            headless: 'new',
+            executablePath: process.env.CHROME_PATH,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
     } else {
         return puppeteer.connect({
