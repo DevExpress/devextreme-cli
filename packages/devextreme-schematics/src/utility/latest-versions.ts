fe475ab8065ebfe910b2875aecf753a190bc0c6b
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2023-06-23 11:59:55 +0400

    cap the theme versions

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '23.1.3',
-  'devextreme-angular': '23.1.3',
+  'devextreme': '^23.1.3',
+  'devextreme-angular': '^23.1.3',
   'devextreme-cli': 'latest'
 };
