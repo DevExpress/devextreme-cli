106ba55c27bb6a37b52421aaae0332ca1894cf88
alexlavrov <alex.s.lavrov@gmail.com>
2023-09-04 20:29:51 +0400

    Bump devextreme version to 23.1.5

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '^23.1.3',
-    'devextreme-react': '^23.1.3',
-    'devextreme-vue': '^23.1.3',
+    'devextreme': '^23.1.5',
+    'devextreme-react': '^23.1.5',
+    'devextreme-vue': '^23.1.5',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
