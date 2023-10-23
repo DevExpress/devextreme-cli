8fb2d98225f9ae5dc0ef3b60432a3075ab640931
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2023-10-23 18:11:42 +0400

    Bump devextreme version to 23.1.6 (#854)

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '^23.1.5',
-  'devextreme-angular': '^23.1.5',
+  'devextreme': '^23.1.6',
+  'devextreme-angular': '^23.1.6',
   'devextreme-cli': 'latest'
 };
