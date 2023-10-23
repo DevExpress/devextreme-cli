22cfc6ab158f3f439455aa36a358a7888f521206
Lykoi18 <aiden.ghost666@gmail.com>
2023-10-23 12:28:37 +0400

    Bump devextreme version to 23.1.6

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '^23.1.5',
-    'devextreme-react': '^23.1.5',
-    'devextreme-vue': '^23.1.5',
+    'devextreme': '^23.1.6',
+    'devextreme-react': '^23.1.6',
+    'devextreme-vue': '^23.1.6',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
