bc8533eceddef23a2f88ca3e288cad77d09c1b43
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2023-11-21 14:58:32 +0400

    Update version

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '^23.1.6',
-    'devextreme-react': '^23.1.6',
-    'devextreme-vue': '^23.1.6',
+    'devextreme': '23.2-next',
+    'devextreme-react': '23.2-next',
+    'devextreme-vue': '23.2-next',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
