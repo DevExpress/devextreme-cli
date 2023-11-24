898fc830a8af25f6fa9e7e8df21e4eed83e08d7d
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2023-11-24 11:33:13 +0400

    Update

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '^23.1.6',
-  'devextreme-angular': '^23.1.6',
+  'devextreme': '23.2-next',
+  'devextreme-angular': '23.2-next',
   'devextreme-cli': 'latest'
 };
