22dae0e1419c564cb767fa597e93ec47f4925702
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2023-12-18 15:04:25 +0400

    Update version to stable

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '23.2-next',
-    'devextreme-react': '23.2-next',
-    'devextreme-vue': '23.2-next',
+    'devextreme': '23.2.3',
+    'devextreme-react': '23.2.3',
+    'devextreme-vue': '23.2.3',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '23.2-next',
-  'devextreme-angular': '23.2-next',
+  'devextreme': '23.2.3',
+  'devextreme-angular': '23.2.3',
   'devextreme-cli': 'latest'
 };
