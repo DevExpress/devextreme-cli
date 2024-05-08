c5c5b1f9197279e729415929c89487bcfff3c98c
Roman Semenov <roman.semenov@devexpress.com>
2024-05-08 16:34:27 +0400

    Bump to 23.2.6 (#887)

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '23.2.4',
-    'devextreme-react': '23.2.4',
-    'devextreme-vue': '23.2.4',
+    'devextreme': '23.2.6',
+    'devextreme-react': '23.2.6',
+    'devextreme-vue': '23.2.6',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
