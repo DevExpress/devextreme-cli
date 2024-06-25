e483e583b0121e3e8446de682903b0b3d466760e
Rochmar Nicolas (DevExpress) <rochmar.nicolas@devsoft.ph>
2024-06-25 13:16:33 +0800

    Enable React Strict Mode

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '24.1.3',
-    'devextreme-react': '24.1.3',
-    'devextreme-vue': '24.1.3',
+    'devextreme': '24.1.4',
+    'devextreme-react': '24.1.4',
+    'devextreme-vue': '24.1.4',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
