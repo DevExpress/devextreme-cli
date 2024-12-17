58036011da0c418358ba1f0c7a6d91f3ac98049f
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-12-17 10:33:52 +0400

    update dx version

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '24.2.2-beta',
-    'devextreme-react': '24.2.2-beta',
-    'devextreme-vue': '24.2.2-beta',
+    'devextreme': '24.2.3',
+    'devextreme-react': '24.2.3',
+    'devextreme-vue': '24.2.3',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
