79934be99e07cc5782d3aafb404691cfcfddbf68
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2025-01-17 10:28:15 +0200

    import screen from testing-lib/dom

--- packages/devextreme-cli/src/templates/cra-template-typescript/template/src/App.test.tsx
+++ packages/devextreme-cli/src/templates/cra-template-typescript/template/src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
 import App from './App';
 
 test('renders learn react link', () => {
