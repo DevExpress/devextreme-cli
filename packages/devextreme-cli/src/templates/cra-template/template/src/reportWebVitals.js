da7ce73e400284f376b58c46ce498aa8b642947a
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2025-01-17 11:09:35 +0200

    web vitals

--- packages/devextreme-cli/src/templates/cra-template/template/src/reportWebVitals.js
+++ packages/devextreme-cli/src/templates/cra-template/template/src/reportWebVitals.js
@@ -1,11 +1,11 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import('web-vitals').then(({ onLCP, onINP, onCLS, onFCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onINP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onFCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
