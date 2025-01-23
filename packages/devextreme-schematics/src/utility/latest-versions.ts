0ee34cf8e2849927a175be7685307a7b2eb84a03
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2025-01-23 12:16:53 +0200

    Upgrade to DevExtreme 24.2.4

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '24.2.3',
-  'devextreme-angular': '24.2.3',
+  'devextreme': '24.2.4',
+  'devextreme-angular': '24.2.4',
   'devextreme-cli': 'latest'
 };
