9ebf014487713d88a5d49aa5f2fb3dc8a1f91077
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2025-02-20 14:11:12 +0400

    Upgrade to DevExtreme 24.2.5 (#923)
    * Upgrade to DevExtreme 24.2.4
    
    * it's 24.2.5 time
    
    * try unpin tooling version
    
    * try update baseline screenshots

--- packages/devextreme-cli/src/utility/extract-tooling-version.js
+++ packages/devextreme-cli/src/utility/extract-tooling-version.js
@@ -1,11 +1,10 @@
 const toolingVersionOptionName = 'tooling-version';
-const defaultVersion = '5.0.1';
 
 const extractToolingVersion = (commandOptions) => {
     if(commandOptions && commandOptions[toolingVersionOptionName]) {
         return `@${commandOptions[toolingVersionOptionName]}`;
     }
-    return `@${defaultVersion}`;
+    return '';
 };
 
 module.exports = {
