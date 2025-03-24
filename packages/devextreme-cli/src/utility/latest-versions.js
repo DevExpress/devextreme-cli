274156cddfc437f2b025e2e705ec106887d98d16
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-03-24 18:15:01 +0400

    Upgrade to DevExtreme 24.2.6

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '24.2.5',
-    'devextreme-react': '24.2.5',
-    'devextreme-vue': '24.2.5',
+    'devextreme': '24.2.6',
+    'devextreme-react': '24.2.6',
+    'devextreme-vue': '24.2.6',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '24.2.5',
-  'devextreme-angular': '24.2.5',
+  'devextreme': '24.2.6',
+  'devextreme-angular': '24.2.6',
   'devextreme-cli': 'latest'
 };
