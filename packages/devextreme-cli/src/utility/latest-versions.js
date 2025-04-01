b30c14917d52bdee38d908767dabd1637b0345c0
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-04-01 17:14:40 +0400

    upgrade to Devextreme 25.1-next

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '24.2.5',
-    'devextreme-react': '24.2.5',
-    'devextreme-vue': '24.2.5',
+    'devextreme': '25.1-next',
+    'devextreme-react': '25.1-next',
+    'devextreme-vue': '25.1-next',
     'create-vite': '6.3.1',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '24.2.5',
-  'devextreme-angular': '24.2.5',
+  'devextreme': '25.1-next',
+  'devextreme-angular': '25.1-next',
   'devextreme-cli': 'latest'
 };
