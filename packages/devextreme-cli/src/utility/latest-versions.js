e21b852f17aab1de55c10f1102de610a8e8af742
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-04-04 12:23:22 +0400

    change version

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '25.1-next',
-    'devextreme-react': '25.1-next',
-    'devextreme-vue': '25.1-next',
+    'devextreme': '25.1.1-alpha-25093-1935',
+    'devextreme-react': '25.1.1-alpha-25093-1935',
+    'devextreme-vue': '25.1.1-alpha-25093-1935',
     'create-vite': '6.3.1',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
