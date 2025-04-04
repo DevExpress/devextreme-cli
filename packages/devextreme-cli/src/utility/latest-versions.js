3a9528379649f438712c85ccf117b9aa95f1478b
Aliullov Vlad <91639107+GoodDayForSurf@users.noreply.github.com>
2025-04-04 15:04:39 +0400

    Upgrade Devextreme version (#943)
    * upgrade to Devextreme 25.1.1-alpha

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '24.2.5',
-    'devextreme-react': '24.2.5',
-    'devextreme-vue': '24.2.5',
+    'devextreme': '25.1.1-alpha',
+    'devextreme-react': '25.1.1-alpha',
+    'devextreme-vue': '25.1.1-alpha',
     'create-vite': '6.3.1',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '24.2.5',
-  'devextreme-angular': '24.2.5',
+  'devextreme': '25.1.1-alpha',
+  'devextreme-angular': '25.1.1-alpha',
   'devextreme-cli': 'latest'
 };
