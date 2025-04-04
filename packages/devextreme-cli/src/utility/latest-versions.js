358a517cd9a0e836b9b0d78458b20c24190b4166
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-04-04 12:50:01 +0400

    change version next to alpha

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,8 +1,8 @@
 const packageJson = require('../../package.json');
 module.exports = {
-    'devextreme': '25.1-alpha',
-    'devextreme-react': '25.1-alpha',
-    'devextreme-vue': '25.1-alpha',
+    'devextreme': '25.1.1-alpha',
+    'devextreme-react': '25.1.1-alpha',
+    'devextreme-vue': '25.1.1-alpha',
     'create-vite': '6.3.1',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
