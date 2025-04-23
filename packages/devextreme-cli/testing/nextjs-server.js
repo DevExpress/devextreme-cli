971a27268945623e8d105c168bbc341623f5ee28
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-04-23 22:04:47 +0400

    clean code

--- packages/devextreme-cli/testing/nextjs-server.js
+++ packages/devextreme-cli/testing/nextjs-server.js
@@ -9,12 +9,8 @@ module.exports = class NextJsServer {
     async start() {
         ({ proc: this.proc } = await runCommand('npm', ['run', 'start'], {
             cwd: this.env.appPath,
-            detached: true,
-            // https://github.com/facebook/create-react-app/issues/3657
-            env: Object.assign(process.env, { CI: false })
+            detached: true
         }));
-
-        // await new Promise(resolve => setTimeout(resolve, 5000));
     }
 
     stop() {
