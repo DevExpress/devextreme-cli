41f3cdce056a94159e507c4e71573265ca7ecb1b
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2025-04-28 19:14:49 +0300

    import path

--- packages/devextreme-cli/src/templates/nextjs/application/src/components/change-password-form/ChangePasswordForm.tsx
+++ packages/devextreme-cli/src/templates/nextjs/application/src/components/change-password-form/ChangePasswordForm.tsx
@@ -12,7 +12,7 @@ import Form, {
 import LoadIndicator from 'devextreme-react/load-indicator';
 import notify from 'devextreme/ui/notify';
 <%=#isTypeScript%>import { ValidationCallbackData } from 'devextreme-react/common';<%=/isTypeScript%>
-import { changePassword } from '../../api/auth';
+import { changePassword } from '@/app/actions/auth';
 
 export default function ChangePasswordForm() {
   const router = useRouter();
