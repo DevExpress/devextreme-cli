27ebfef5cd7a6d0572483ffb888410571787deb7
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2025-04-28 13:06:16 +0300

    latest versions

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '24.2.7',
-  'devextreme-angular': '24.2.7',
+  'devextreme': '24.2-next',
+  'devextreme-angular': '24.2-next',
   'devextreme-cli': 'latest'
 };
