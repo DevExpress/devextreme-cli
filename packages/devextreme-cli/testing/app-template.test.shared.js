87f83c631fba156e4b420accd02471ab5ba9712c
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2025-04-29 17:59:40 +0300

    threshold

--- packages/devextreme-cli/testing/app-template.test.shared.js
+++ packages/devextreme-cli/testing/app-template.test.shared.js
@@ -84,7 +84,7 @@ module.exports = (env, { port = 8080, urls = {} } = {}) => {
                                 await page.waitForTimeout(500);
                             }
 
-                            const customConfig = { threshold: 0.012 };
+                            const customConfig = { threshold: 0.02 };
 
                             function compareSnapshot(image, name) {
                                 expect(image).toMatchImageSnapshot({
