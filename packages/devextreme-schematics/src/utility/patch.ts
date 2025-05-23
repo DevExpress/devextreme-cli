2630cbe0e94623535d7cd15f75a947632db04fb3
sergey-kras <vip.41243@gmail.com>
2025-05-23 16:56:35 +0500

    fix: reverted patch

--- packages/devextreme-schematics/src/utility/patch.ts
+++ packages/devextreme-schematics/src/utility/patch.ts
@@ -1,15 +1,6 @@
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 export class PatchNodePackageInstallTask extends NodePackageInstallTask {
-    // TODO: remove this patch before merge
-    constructor() {
-        super({
-            packageManager: 'npm',
-            packageName: '--legacy-peer-deps',
-            allowScripts: true,
-        });
-    }
-
     toConfiguration(): any {
         const rootConfigurations = super.toConfiguration();
         const customOptions = Object.assign({}, rootConfigurations.options, { allowScripts: true });
