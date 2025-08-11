d3ba6bfa58ba26345f8f793ba97d16ea22305c93
alexlavrov <alex.s.lavrov@gmail.com>
2025-08-11 17:10:59 +0400

    25.1.4

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '25.1.3',
-  'devextreme-angular': '25.1.3',
+  'devextreme': '25.1.4',
+  'devextreme-angular': '25.1.4',
   'devextreme-cli': 'latest'
 };
