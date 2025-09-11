fd5df9010671c0860f7fddffbf36f4a57c988cd9
Arman Jivanyan <arman.jivanyan@devexpress.com>
2025-09-11 15:56:01 +0400

    25.1.5

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,7 +1,7 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '25.1.4',
-  'devextreme-angular': '25.1.4',
+  'devextreme': '25.1.5',
+  'devextreme-angular': '25.1.5',
   'devextreme-cli': 'latest',
   'sass-embedded': '1.66.0'
 };
