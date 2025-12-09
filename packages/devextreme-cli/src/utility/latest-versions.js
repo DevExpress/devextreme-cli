ba96d97abe18063b8f5631a3bff39c9aae2465b3
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2025-12-09 14:54:21 +0200

    bump scaffolders

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -3,10 +3,10 @@ module.exports = {
     'devextreme': '25.1.7',
     'devextreme-react': '25.1.7',
     'devextreme-vue': '25.1.7',
-    'create-vite': '7.0.0',
-    'create-vue': '3.17.0',
-    'create-next-app': '15.3.4',
-    'jose': '6.0.11',
+    'create-vite': '8.2.0',
+    'create-vue': '3.18.3',
+    'create-next-app': '16.0.8',
+    'jose': '6.1.3',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
 };
