09cfbfc87aff3e167f2f96759c4259173dd3a2fc
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2025-12-09 17:51:06 +0200

    roll back vue

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -4,7 +4,7 @@ module.exports = {
     'devextreme-react': '25.1.7',
     'devextreme-vue': '25.1.7',
     'create-vite': '8.2.0',
-    'create-vue': '3.18.3',
+    'create-vue': '3.17.0',
     'create-next-app': '16.0.8',
     'jose': '6.1.3',
     'devextreme-cli': packageJson.version,
