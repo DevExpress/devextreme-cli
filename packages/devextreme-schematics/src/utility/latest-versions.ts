2e3259b22bb101e361f6f3aaca5fef3063c25443
Alex Lavrov <36633600+alexslavr@users.noreply.github.com>
2026-08-10 16:35:05 +0400

    Bump devextreme to 26.1.4 (#1222)

--- packages/devextreme-schematics/src/utility/latest-versions.ts
+++ packages/devextreme-schematics/src/utility/latest-versions.ts
@@ -1,7 +1,7 @@
 // TODO: implement
 export const latestVersions = {
-  'devextreme': '26.1.3',
-  'devextreme-angular': '26.1.3',
+  'devextreme': '26.1.4',
+  'devextreme-angular': '26.1.4',
   'devextreme-cli': 'latest',
   'sass-embedded': '1.66.0'
 };
