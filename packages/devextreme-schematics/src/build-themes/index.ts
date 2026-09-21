9cbcf85da3c256b26029a735e59479b6665321f2
Andrei Vorobev <738482+vorobey@users.noreply.github.com>
2026-09-21 14:38:37 +0300

    Schematics: fix test

--- packages/devextreme-schematics/src/build-themes/index.ts
+++ packages/devextreme-schematics/src/build-themes/index.ts
@@ -1,14 +1,30 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
 import { spawnSync } from 'child_process';
 
+function hasBuildThemesScript(host: Tree) {
+  const packageJson = host.read('./package.json');
+
+  if (!packageJson) {
+    return false;
+  }
+
+  const scripts = JSON.parse(packageJson.toString())['scripts'];
+
+  return !!scripts && !!scripts['build-themes'];
+}
+
 export default function(): Rule {
   return (host: Tree) => {
-    const isWin = /^win/.test(process.platform);
-    spawnSync('npm', ['run', 'build-themes'], {
-      stdio: 'inherit',
-      windowsVerbatimArguments: true,
-      shell: !isWin ? true : false
-    });
+    if (hasBuildThemesScript(host)) {
+      const isWin = /^win/.test(process.platform);
+
+      spawnSync('npm', ['run', 'build-themes'], {
+        stdio: 'inherit',
+        windowsVerbatimArguments: true,
+        shell: !isWin
+      });
+    }
+
     return host;
   };
 }
